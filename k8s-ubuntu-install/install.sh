7f5f77493378a7023828d77f4f7f215e563a7cdf
Marian Craciun <mariancraciun1983@users.noreply.github.com>
2023-03-10 18:15:15 +0100

    Update install.sh

--- k8s-ubuntu-install/install.sh
+++ k8s-ubuntu-install/install.sh
@@ -1,137 +1,117 @@
 #!/bin/bash
 
+set -e
+
 export DEBIAN_FRONTEND="noninteractive"
-export KUBERNETES_VERSION="1.22.6-00"
+export CONTAINERD_VERSION="1.6.18"
+export KUBERNETES_VERSION="1.26.2"
 export DPKG_LOCK_TIMOUT="-1"
 
 #############################################
 # Disable UFW cause of Problems with Docker #
 #############################################
 echo '> Disable ufw and purge iptables ...'
-sudo systemctl stop ufw.service
-sudo systemctl disable ufw.service
-# sudo iptables -F
+systemctl stop ufw.service
+systemctl disable ufw.service
+# iptables -F
 
 ########################################
 # Letting iptables see bridged traffic #
 ########################################
 echo '> Letting iptables see bridged traffic ...'
-test -e /etc/modules-load.d/k8s.conf || cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+test -e /etc/modules-load.d/k8s.conf || cat <<EOF | tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
-
-test -e /etc/sysctl.d/k8s.conf || cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+test -e /etc/sysctl.d/k8s.conf || cat <<EOF | tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward                 = 1
 EOF
-
-sudo sysctl --system
-
-#################################################################
-# Installs packages needed to use the Kubernetes apt repository #
-#################################################################
-echo '> Installs packages needed to use the Kubernetes apt repository  ...'
-sudo apt-get -o DPkg::Lock::Timeout=${DPKG_LOCK_TIMOUT} update
-sudo apt-get -o DPkg::Lock::Timeout=${DPKG_LOCK_TIMOUT} install -y apt-transport-https ca-certificates curl gnupg lsb-release
-
-####################################
-# Configures Containerd #
-####################################
-echo '> Configure Containerd ...'
-sudo test -d /etc/containerd || sudo mkdir /etc/containerd
-sudo test -e /etc/containerd/config.toml || sudo cat > /etc/containerd/config.toml <<EOF
-disabled_plugins = ["cri"]
-
-[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
-
-  [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
-    SystemdCgroup = true
-EOF
-
-#################
-# Install Docker#
-#################
-echo '> Configure Docker ...'
-sudo test -d /etc/docker || sudo mkdir /etc/docker
-sudo cat <<EOF | sudo tee /etc/docker/daemon.json
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
-
-##############################
-## Set up stable repository ##
-##############################
-echo '> Set up stable Docker repository ...'
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-####################################
-## Add Docker’s official GPG key: ##
-####################################
-sudo test -e /usr/share/keyrings/docker-archive-keyring.gpg || curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-################################
-# Update the apt package index #
-################################
-echo '> Update the apt package index ...'
-sudo apt-get -o DPkg::Lock::Timeout=${DPKG_LOCK_TIMOUT} update
-###########################
-## Install Docker Engine ##
-##########################
-echo '> Install Docker Engine ...'
-sudo apt-get -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' -o DPkg::Lock::Timeout=${DPKG_LOCK_TIMOUT} install -y docker-ce docker-ce-cli containerd.io
-
-#############################
-## Start and enable Docker ##
-#############################
-echo '> Start and enable Docker ...'
-sudo systemctl enable docker
-sudo systemctl daemon-reload
-sudo systemctl restart docker
+sysctl --system
 
 ################
 # Disable SWAP #
 ################
 echo '> Disable SWAP ...'
-sudo sed -i~ /swap/d /etc/fstab
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+#################################################################
+# Installs packages needed to use the Kubernetes apt repository
+#################################################################
+echo '> Installs packages needed to use the Kubernetes apt repository  ...'
+apt-get -o DPkg::Lock::Timeout=${DPKG_LOCK_TIMOUT} update
+apt-get -o DPkg::Lock::Timeout=${DPKG_LOCK_TIMOUT} install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release \
+  apparmor \
+  apparmor-profiles \
+  apparmor-utils \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  dirmngr \
+  git \
+  gnupg \
+  gnupg-l10n \
+  gpgsm \
+  gpgv \
+  inotify-tools \
+  linux-virtual-hwe-22.04 \
+  lsb-release \
+  mlocate \
+  ntp \
+  p7zip-full \
+  software-properties-common \
+  wget
+apt purge -y lxd lxd-client snapd
+
+
+############################################
+# Add  apt repository signing key
+############################################
+echo '> Add  apt repository signing key ...'
+test -f /usr/share/keyrings/kubernetes-archive-keyring.gpg || curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+test -f /etc/apt/trusted.gpg.d/docker.gpg || curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+
+####################################
+# Add apt repositories
+####################################
+test -f /etc/apt/sources.list.d/kubernetes.list ||echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+test -f /etc/apt/sources.list.d/docker.list ||echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
+
+####################################
+# Install and Lock Packages
+####################################
+apt update
+apt install -y \
+  kubelet="${KUBERNETES_VERSION}-00"\
+  kubeadm="${KUBERNETES_VERSION}-00"\
+  kubectl="${KUBERNETES_VERSION}-00" \
+  containerd.io="${CONTAINERD_VERSION}-1"
+
+apt-mark -o DPkg::Lock::Timeout=${DPKG_LOCK_TIMOUT} hold kubelet kubeadm kubectl containerd
+
+####################################
+# Configures Containerd
+####################################
+echo '> Configure Containerd ...'
+containerd config default | tee /etc/containerd/config.toml >/dev/null 2>&1
+sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+systemctl enable containerd
+systemctl daemon-reload
+systemctl restart containerd
 
 #####################
 # Configure Kubelet #
 #####################
-sudo test -d /etc/systemd/system/kubelet.service.d || sudo mkdir /etc/systemd/system/kubelet.service.d
-sudo test -e /etc/systemd/system/kubelet.service.d/20-hcloud.conf || sudo cat > /etc/systemd/system/kubelet.service.d/20-hcloud.conf <<EOF
+test -d /etc/systemd/system/kubelet.service.d || mkdir /etc/systemd/system/kubelet.service.d
+test -e /etc/systemd/system/kubelet.service.d/20-hcloud.conf || cat > /etc/systemd/system/kubelet.service.d/20-hcloud.conf <<EOF
 [Service]
 Environment="KUBELET_EXTRA_ARGS=--cloud-provider=external"
 EOF
-
-###################################################
-# Install Kubeadm Kubectl & Kubelet               #
-###################################################
-## Download the Google Cloud public signing key: ##
-###################################################
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-
-########################################
-## Add the Kubernetes apt repository: ##
-########################################
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-############################################################################################
-## Update apt package index, install kubelet, kubeadm and kubectl, and pin their version: ##
-############################################################################################
-sudo apt-get -o DPkg::Lock::Timeout=${DPKG_LOCK_TIMOUT} update
-sudo apt-get -o DPkg::Lock::Timeout=${DPKG_LOCK_TIMOUT} install -y kubelet=${KUBERNETES_VERSION} kubeadm=${KUBERNETES_VERSION} kubectl=${KUBERNETES_VERSION}
-sudo apt-mark -o DPkg::Lock::Timeout=${DPKG_LOCK_TIMOUT} hold kubelet kubeadm kubectl
-
-####################
-## Enable Kubelet ##
-####################
-sudo systemctl enable --now kubelet
+systemctl enable --now kubelet
